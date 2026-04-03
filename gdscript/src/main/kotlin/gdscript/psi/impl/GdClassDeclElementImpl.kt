6854e8944cd4b2a6fd0f1057e2e1789679fa8931
dasTholo <kaitholo@gmail.com>
2026-04-03 09:25:21 +0200

    fix: add getPresentation() to class and signal PSI elements for navigation display

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdClassDeclElementImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdClassDeclElementImpl.kt
@@ -1,7 +1,9 @@
 package gdscript.psi.impl
 
+import GdScriptPluginIcons
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
+import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
 import gdscript.index.stub.GdClassDeclStub
 
@@ -9,6 +11,14 @@ abstract class GdClassDeclElementImpl : StubBasedPsiElementBase<GdClassDeclStub>
 
     constructor(node: ASTNode) : super(node)
     constructor(stub: GdClassDeclStub, nodeType: IStubElementType<*, *>) : super(stub, nodeType)
+
+    override fun getPresentation(): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText(): String? = name
+            override fun getLocationString(): String? = containingFile?.name
+            override fun getIcon(unused: Boolean) = GdScriptPluginIcons.GDScriptIcons.OBJECT
+        }
+    }
 
     override fun toString() = "GdClassDecl"
 
--- gdscript/src/main/kotlin/gdscript/psi/impl/GdClassNamingElementImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdClassNamingElementImpl.kt
@@ -1,7 +1,9 @@
 package gdscript.psi.impl
 
+import GdScriptPluginIcons
 import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
+import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
 import gdscript.index.stub.GdClassNamingStub
 import gdscript.psi.GdClassNaming
@@ -14,6 +16,14 @@ abstract class GdClassNamingElementImpl : StubBasedPsiElementBase<GdClassNamingS
     override fun getName(): String? {
         greenStub?.let { return it.name() }
         return (this as? GdClassNaming)?.classname
+    }
+
+    override fun getPresentation(): ItemPresentation {
+        return object : ItemPresentation {
+            override fun getPresentableText(): String? = name
+            override fun getLocationString(): String? = containingFile?.name
+            override fun getIcon(unused: Boolean) = GdScriptPluginIcons.GDScriptIcons.OBJECT
+        }
     }
 
     override fun toString() = "GdClassNaming"
