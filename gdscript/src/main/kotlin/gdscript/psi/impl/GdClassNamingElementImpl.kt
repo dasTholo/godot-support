21f1281e52303206ebbf5284ffef3afad72d3025
dasTholo <kaitholo@gmail.com>
2026-04-03 09:30:44 +0200

    fix: add getIcon() override for navigation dialog icon display

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdClassNamingElementImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdClassNamingElementImpl.kt
@@ -5,6 +5,7 @@ import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
+import javax.swing.Icon
 import gdscript.index.stub.GdClassNamingStub
 import gdscript.psi.GdClassNaming
 
@@ -22,9 +23,11 @@ abstract class GdClassNamingElementImpl : StubBasedPsiElementBase<GdClassNamingS
         return object : ItemPresentation {
             override fun getPresentableText(): String? = name
             override fun getLocationString(): String? = containingFile?.name
-            override fun getIcon(unused: Boolean) = GdScriptPluginIcons.GDScriptIcons.OBJECT
+            override fun getIcon(unused: Boolean): Icon? = GdScriptPluginIcons.GDScriptIcons.OBJECT
         }
     }
+
+    override fun getIcon(flags: Int): Icon = GdScriptPluginIcons.GDScriptIcons.OBJECT
 
     override fun toString() = "GdClassNaming"
 
--- gdscript/src/main/kotlin/gdscript/psi/impl/GdSignalDeclElementImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdSignalDeclElementImpl.kt
@@ -5,6 +5,7 @@ import com.intellij.extapi.psi.StubBasedPsiElementBase
 import com.intellij.lang.ASTNode
 import com.intellij.navigation.ItemPresentation
 import com.intellij.psi.stubs.IStubElementType
+import javax.swing.Icon
 import gdscript.index.stub.GdSignalDeclStub
 
 abstract class GdSignalDeclElementImpl : StubBasedPsiElementBase<GdSignalDeclStub> {
@@ -16,9 +17,11 @@ abstract class GdSignalDeclElementImpl : StubBasedPsiElementBase<GdSignalDeclStu
         return object : ItemPresentation {
             override fun getPresentableText(): String? = name
             override fun getLocationString(): String? = containingFile?.name
-            override fun getIcon(unused: Boolean) = GdScriptPluginIcons.GDScriptIcons.SIGNAL_MARKER
+            override fun getIcon(unused: Boolean): Icon? = GdScriptPluginIcons.GDScriptIcons.SIGNAL_MARKER
         }
     }
+
+    override fun getIcon(flags: Int): Icon = GdScriptPluginIcons.GDScriptIcons.SIGNAL_MARKER
 
     override fun toString() = "GdSignalDecl"
 
